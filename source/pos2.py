1fa3b0e0a0c6d4d6e29c26ce06c4fddad401db6a
vaidhehi <33127297+vaidhehi@users.noreply.github.com>
2018-10-24 15:03:11 -0700

    POS Tagging - Creating queries by removing certain tags.

--- source/pos2.py
+++ source/pos2.py
@@ -20,30 +20,45 @@ tagger = StanfordPOSTagger(english_model, path_to_jar, encoding='utf-8')
 xl = pd.read_excel('sentence.xlsx', header=None)
 
 print('Running')
-alternative1 = []
+#alternative1 = []
 for row in xl.iterrows():
     text = row[1][0]
     tagged_sent = tagger.tag(text.split())
     df = pd.DataFrame(tagged_sent,columns=['token','Pos-Tag'])
-    alternative1.append(df)
+    df = df.set_index("Pos-Tag")
+    df = df.drop("DT", axis=0)
+    df = df.drop("IN", axis=0)
+    df = df.drop("PRP$", axis=0)
+    #df = df.drop("EX", axis=0)
+    #df = df.drop("LS", axis=0)
+    #df = df.drop("PRP", axis=0)
+    print(df)
+    break
+    #alternative1.append(df)
 
-pickle.dump( alternative1, open( "save.p", "wb" ) )
+#pickle.dump( alternative1, open( "save.p", "wb" ) )
 print('Dumped the first pickle')
-alternative2 = []
+#alternative2 = []
 for row in xl.iterrows():
     text = row[1][1]
     tagged_sent = tagger.tag(text.split())
     df1 = pd.DataFrame(tagged_sent, columns=['token', 'Pos-Tag'])
-    alternative2.append(df1)
+    df1 = df1.set_index("Pos-Tag")
+    df1 = df1.drop("DT", axis=0)
+    df1 = df1.drop("IN", axis=0)
+    df1 = df1.drop("PRP$", axis=0)
+    print(df1)
+    break
+    #alternative2.append(df1)
 
-pickle.dump ( alternative2, open( "save1.p", "wb") )
+#pickle.dump ( alternative2, open( "save1.p", "wb") )
 
-print('Dumped the first pickle')
-with open('save.p', 'rb') as f:
-    data = pickle.load(f)
-    print(data)
+print('Dumped the second pickle')
+#with open('save.p', 'rb') as f:
+#    data = pickle.load(f)
+#    print(data)
 
-with open('save1.p', 'rb') as f:
-    data = pickle.load(f)
-    print(data)
-print('Dumped the first pickle')
+#with open('save1.p', 'rb') as f:
+#    data = pickle.load(f)
+#    print(data)
+#print('Dumped the first pickle')
