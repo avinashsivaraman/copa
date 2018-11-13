7300b9e28300d97bffd07ba6f725d12bb447e1f4
kaviya kalaichelvan <kkavi381@gmail.com>
2018-11-12 22:46:08 -0700

    Split query

--- source/memo-assert.py
+++ source/memo-assert.py
@@ -1,6 +1,11 @@
 import pickle
 
-
+def splitquery(query):
+    ind = 0
+    for element in query:
+        if '.' in element:
+            return query[0:ind]+query[ind].split('.')+query[ind+1:]
+        ind = ind+1
 
 with open('memo-sentence-full-bing.p', 'rb') as f:
     correct = pickle.load(f)
@@ -8,8 +13,8 @@ with open('memo-sentence-full-bing.p', 'rb') as f:
 
 for k, v in correct.items():
     every = v["sentences1"]
-    queryParameter = v["queryparameter1"]
-    # TODO: Kaviya Implement the algo to split and join the array
+    queryParameter1 = splitquery(v["queryparameter1"])
+    queryParameter2 = splitquery(v["queryparameter2"])
     i = 0
     for each in every:
         print(i+1, each)
